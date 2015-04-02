8f698e4c945151fcc2a23c429c594279e5dae8ab
luanrabelo <luanrabelo@outlook.com>
2015-04-02 10:38:58 -0300

    Create home.php

---
+++ home.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Untitled Document</title>
+<body>
+<table width="700" align="center">
+  <tr>
+    <td><p>Olá <?php echo $_SESSION['nome']?>, Seja bem-vindo ao Aquasoft, agradecemos à você e sua instituição (<?php echo $_SESSION['instituicao']?>), em especial ao seu departamento (<?php echo $_SESSION['departamento']?>), enviamos para seu e-mail (<?php echo $_SESSION['email']?>) mais informações sobre o período de testes do programa e de como você pode nos ajudar à melhorá-lo.</p>
+    <p>Mais informações por email</p>
+    <p><a href="mailto:luanrabelo@outlook.com">Luan Rabelo</a></p></td>
+  </tr>
+</table>
+</body>
+</html>
